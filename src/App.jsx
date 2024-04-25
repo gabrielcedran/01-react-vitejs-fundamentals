0d8afdb4b503a69f511afb12974353631666a7df
Gabriel Cedran <gabrielcedran@gmail.com>
2024-04-25 21:13:22 +0100

    Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
 import { Post } from "./Post";
 import { Header } from "./components/Header";
 import './global.css';
+import styles from './App.module.css';
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post 
-        author="Don" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda molestias ad dolorum optio! Facere doloribus itaque beatae maiores, hic fugiat repellendus eveniet sed aperiam. Voluptate sit doloribus nemo sunt explicabo!"
-      />
-      <Post 
-        author="Bob" 
-        content="Another beautiful post!"
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main> 
+          <Post 
+            author="Don" 
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda molestias ad dolorum optio! Facere doloribus itaque beatae maiores, hic fugiat repellendus eveniet sed aperiam. Voluptate sit doloribus nemo sunt explicabo!"
+          />
+          <Post 
+            author="Bob" 
+            content="Another beautiful post!"
+          />
+        </main>
+      </div>
+
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,25 @@
+import styles from './Sidebar.module.css';
+import { PencilLine } from 'phosphor-react'
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img 
+                className={styles.backgroundImage}
+                src="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?q=40&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+            />
+            <div className={styles.profile}>
+                <img 
+                    className={styles.avatar}
+                    src="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?q=40&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+                />
+                <strong>Don Bob</strong>
+                <span>Web developer</span>
+            </div>
+
+            <footer>
+                <a href="#"><PencilLine size={20} />Edit Profile</a>
+            </footer>
+        </aside>
+    )
+}
