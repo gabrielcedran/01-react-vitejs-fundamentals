6814f27b2c0a335ab7b88dc1bd5fa8e3ab496f1c
Gabriel Cedran <gabrielcedran@gmail.com>
2024-04-27 20:18:47 +0100

    Componentise avatar + improve responsiveness

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import styles from './Sidebar.module.css';
 import { PencilLine } from 'phosphor-react'
 
@@ -9,10 +10,7 @@ export function Sidebar() {
                 src="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?q=40&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
             />
             <div className={styles.profile}>
-                <img 
-                    className={styles.avatar}
-                    src="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?q=40&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
-                />
+                <Avatar src="https://github.com/gabrielcedran.png" />
                 <strong>Don Bob</strong>
                 <span>Web developer</span>
             </div>
